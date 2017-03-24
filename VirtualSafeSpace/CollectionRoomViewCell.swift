d18b60f8cd10b3b3a0f7181573b1cd477b288914
Stacey Vetzal <stacey@vetzal.com>
2017-03-24 15:18:49 -0400

    live list of rooms

---
+++ VirtualSafeSpace/CollectionRoomViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionRoomViewCell.swift
+//  VirtualSafeSpace
+//
+//  Created by Stacey Vetzal on 2017-03-24.
+//  Copyright © 2017 Stacey Vetzal. All rights reserved.
+//
+
+import UIKit
+
+class CollectionRoomViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
